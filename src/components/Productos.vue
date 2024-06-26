fccd6a6fbd7ba7a480802999d0e22e4fc6850cd5
yecamfer <yeseniacampo2013@gmail.com>
2024-06-26 13:00:57 -0500

    Estilos css al componente de productos

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -6,7 +6,7 @@
                 <img class="foto" src="`/imagenes/${producto.imagen}`" alt="Producto">
                 <div class="informacion">
                     <h3 class="titulo">
-                        {{ producto.nombre }}
+                        {{ producto.producto }}
                     </h3>
                     <p class="descrpicion">
                         {{ producto.descrpicion }}
@@ -36,3 +36,51 @@ watchEffect(() => {
     producto.value = listaProductos.filter((producto) => producto.categoria ===props.categoria)
 })
 </script>
+
+<style scoped>
+    .contenedor{
+        padding: 3rem;
+        margin-bottom: 2rem;       
+    }
+    .productos{
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 2rem;
+    }
+    .producto{
+        display: flex;
+        align-items: center;
+    }
+    .informacio{
+        padding: 15px;
+    }
+    .titulo{
+        font-size: 1.25rem;
+    }
+    .descripcion{
+        color: gray;
+        word-wrap: break-word;
+    }
+    .foto{
+        max-width: calc(100% / 4);
+        border-radius: 20px;
+        box-shadow: 0px 2px 4px rgb(77, 76, 76);    
+    }
+    .precio{
+        color: #f9a72b;
+        font-weight: bold;
+        font-size: 1rem;
+    }
+
+    @media(max-width: 1024px) and (min-width: 768px) {
+        .productos {
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
+
+    @media (max-width: 768px) {
+        .productos {
+            grid-template-columns: repeat(1, 1 fr);
+        }
+    }
+    </style>
